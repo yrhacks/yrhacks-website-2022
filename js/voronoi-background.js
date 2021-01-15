aa28c93cce52efa5829a9a2f875cb33e43a822f9
Shari Sun <shari09cxxcr@gmail.com>
2021-01-15 01:02:18 -0500

    added resize listener

--- js/voronoi-background.js
+++ js/voronoi-background.js
@@ -24,16 +24,24 @@ VoronoiPoint.colourRamp = d3.piecewise(
 
 
 const main = document.getElementById("main-voronoi");
-main.width = document.documentElement.clientWidth;
-main.height = window.innerHeight;
-
 const mid = document.getElementById("mid-voronoi");
-mid.width = document.documentElement.clientWidth;
-mid.height = document.getElementById("register").getBoundingClientRect().height;
-
 const footer = document.getElementById("footer-voronoi");
-footer.width = document.documentElement.clientWidth;
-footer.height = document.getElementById("footer").getBoundingClientRect().height;
+
+
+const setCanvasSizes = () => {
+  main.width = document.documentElement.clientWidth;
+  main.height = window.innerHeight;
+
+  mid.width = document.documentElement.clientWidth;
+  mid.height = document.getElementById("register").getBoundingClientRect().height;
+
+  footer.width = document.documentElement.clientWidth;
+  footer.height = document.getElementById("footer").getBoundingClientRect().height;
+};
+
+setCanvasSizes();
+
+window.addEventListener('resize', setCanvasSizes);
 
 const canvases = [main, mid, footer];
 const redraws = canvases.map(canvas => {
